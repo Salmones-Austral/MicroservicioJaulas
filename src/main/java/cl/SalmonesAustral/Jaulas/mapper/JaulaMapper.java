dcc3677d5fefc15a7f2ba898766061de85f14fb9
Crainsver <cristobal21rc@gmail.com>
2026-05-29 12:02:31 -0400

    Commit-falta-crear-metodos-para-recivir-mensajes

--- src/main/java/cl/SalmonesAustral/Jaulas/mapper/JaulaMapper.java
+++ src/main/java/cl/SalmonesAustral/Jaulas/mapper/JaulaMapper.java
@@ -23,12 +23,7 @@ public class JaulaMapper {
                 request.criaderoId()
         );
     }
-     /*@NotBlank(message = "Código no puede ser vacío") String codigo,
-                @PositiveOrZero(message = "Capacidad máxima no puede ser negativa") int capacidadMaxima,
-                @PositiveOrZero(message = "Cantidad actual no puede ser negativa") int cantidadActual,
-                @NotBlank(message = "Activa no puede ser vacía") Boolean activa,
-                @NotBlank(message = "Criadero ID no puede ser vacío") Long criaderoId) {
-*/
+
     /**
      * Convierte DTO de actualización a modelo Jaula (PUT)
      * El ID viene desde el path (controller)
